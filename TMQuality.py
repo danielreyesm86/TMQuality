7f736022ee54881cc270b05230d0dde075fa4988
danielreyesm86 <danielreyesm86@gmail.com>
2026-08-01 15:42:08 -0400

    Update TMQuality.py

--- TMQuality.py
+++ TMQuality.py
@@ -50,11 +50,7 @@ from reportlab.lib.pagesizes import letter
 from reportlab.lib.styles import ParagraphStyle, getSampleStyleSheet
 from reportlab.lib.units import cm
 from reportlab.platypus import (
-    Paragraph,
-    SimpleDocTemplate,
-    Spacer,
-    Table,
-    TableStyle,
+    Paragraph, SimpleDocTemplate, Spacer, Table, TableStyle, Image
 )
 
 
